80868b6126f3c21b60c2cf732789378bb0e57db1
Dmitrii <Dimabmwmw@mail.ru>
2023-03-16 12:24:44 +0500

    Добавьте файлы проекта.

---
+++ HMWRK 21/Temple.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+// Кодировка UTF-8
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n, x, y;
+
+	std::cout << "Введите два числа -> ";
+	std::cin >> x >> y;
+
+	int* px = &x;
+	int* py = &y;
+
+	std::cout << "\nСреднее арифметическое чисел " << x << " и " << y << " = " << (*px + *py) / 2 << std::endl;
+
+
+	return 0;
+}
